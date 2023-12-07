9f8ed64a5141ffeb1f68cfce8850d6e1202e0982
Antoine Toubhans <antoinet@sicara.com>
2023-12-07 18:02:40 +0100

    Move speed zone to zone utils

--- constants.py
+++ constants.py
@@ -8,9 +8,3 @@ STRAVA_PARAMS_FILE = "strava.yaml"
 # ------ Running
 VMA_KMH = 18.5  # km/h
 VMA = VMA_KMH * 1000 / 3600
-Z_1_2 = 0.61 * VMA  # Z1 = EF
-Z_2_3 = 0.75 * VMA  # Z2 = Footing
-Z_3_4 = 0.85 * VMA  # Z3 = Seuil Anaerobie
-Z_4_5 = 0.92 * VMA  # Z4 = AS 10
-Z_5_6 = 0.97 * VMA  # Z5 = VMA Longue
-Z_6_7 = 1.05 * VMA  # Z6 = VMA Moy / Z7 = VM Courte
--- zone_utils.py
+++ zone_utils.py
@@ -1,4 +1,11 @@
-from constants import Z_1_2, Z_2_3, Z_3_4, Z_4_5, Z_5_6, Z_6_7
+from constants import VMA
+
+SPEED_ZONE_1_2 = 0.61 * VMA  # Z1 = EF
+SPEED_ZONE_2_3 = 0.75 * VMA  # Z2 = Footing
+SPEED_ZONE_3_4 = 0.85 * VMA  # Z3 = Seuil Anaerobie
+SPEED_ZONE_4_5 = 0.92 * VMA  # Z4 = AS 10
+SPEED_ZONE_5_6 = 0.97 * VMA  # Z5 = VMA Longue
+SPEED_ZONE_6_7 = 1.05 * VMA  # Z6 = VMA Moy / Z7 = VM Courte
 
 
 def format_speed_ms_to_per_km_str(speed_in_ms: int | float) -> str:
@@ -13,27 +20,27 @@ def format_speed_ms_to_per_km_str(speed_in_ms: int | float) -> str:
     return f"{minutes}:{seconds:02}"
 
 
-Z_1_2_STR = format_speed_ms_to_per_km_str(Z_1_2)
-Z_2_3_STR = format_speed_ms_to_per_km_str(Z_2_3)
-Z_3_4_STR = format_speed_ms_to_per_km_str(Z_3_4)
-Z_4_5_STR = format_speed_ms_to_per_km_str(Z_4_5)
-Z_5_6_STR = format_speed_ms_to_per_km_str(Z_5_6)
-Z_6_7_STR = format_speed_ms_to_per_km_str(Z_6_7)
+SPEED_ZONE_1_2_STR = format_speed_ms_to_per_km_str(SPEED_ZONE_1_2)
+SPEED_ZONE_2_3_STR = format_speed_ms_to_per_km_str(SPEED_ZONE_2_3)
+SPEED_ZONE_3_4_STR = format_speed_ms_to_per_km_str(SPEED_ZONE_3_4)
+SPEED_ZONE_4_5_STR = format_speed_ms_to_per_km_str(SPEED_ZONE_4_5)
+SPEED_ZONE_5_6_STR = format_speed_ms_to_per_km_str(SPEED_ZONE_5_6)
+SPEED_ZONE_6_7_STR = format_speed_ms_to_per_km_str(SPEED_ZONE_6_7)
 
 
 def get_speed_zone_label(speed_in_ms: int | float) -> str:
     match speed_in_ms:
-        case speed_in_ms if speed_in_ms > Z_6_7:
-            return f"Z7 (> {Z_6_7_STR})"
-        case speed_in_ms if speed_in_ms > Z_5_6:
-            return f"Z6 ({Z_5_6_STR}-{Z_6_7_STR})"
-        case speed_in_ms if speed_in_ms > Z_4_5:
-            return f"Z5 ({Z_4_5_STR}-{Z_5_6_STR})"
-        case speed_in_ms if speed_in_ms > Z_3_4:
-            return f"Z4 ({Z_3_4_STR}-{Z_4_5_STR})"
-        case speed_in_ms if speed_in_ms > Z_2_3:
-            return f"Z3 ({Z_2_3_STR}-{Z_3_4_STR})"
-        case speed_in_ms if speed_in_ms > Z_1_2:
-            return f"Z2 ({Z_1_2_STR}-{Z_2_3_STR})"
+        case speed_in_ms if speed_in_ms > SPEED_ZONE_6_7:
+            return f"Z7 (> {SPEED_ZONE_6_7_STR})"
+        case speed_in_ms if speed_in_ms > SPEED_ZONE_5_6:
+            return f"Z6 ({SPEED_ZONE_5_6_STR}-{SPEED_ZONE_6_7_STR})"
+        case speed_in_ms if speed_in_ms > SPEED_ZONE_4_5:
+            return f"Z5 ({SPEED_ZONE_4_5_STR}-{SPEED_ZONE_5_6_STR})"
+        case speed_in_ms if speed_in_ms > SPEED_ZONE_3_4:
+            return f"Z4 ({SPEED_ZONE_3_4_STR}-{SPEED_ZONE_4_5_STR})"
+        case speed_in_ms if speed_in_ms > SPEED_ZONE_2_3:
+            return f"Z3 ({SPEED_ZONE_2_3_STR}-{SPEED_ZONE_3_4_STR})"
+        case speed_in_ms if speed_in_ms > SPEED_ZONE_1_2:
+            return f"Z2 ({SPEED_ZONE_1_2_STR}-{SPEED_ZONE_2_3_STR})"
         case _:
-            return f"Z1 (<{Z_1_2_STR})"
+            return f"Z1 (<{SPEED_ZONE_1_2_STR})"
